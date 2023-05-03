f3014af3854de3c30b7b82f2d032cdb5ecfa1de3
Daniel Do <do2daniel@yahoo.com>
2023-05-03 03:10:50 -0700

    Added new object call to small spaceship

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -7,7 +7,7 @@ class Play extends Phaser.Scene {
         // load images/tile sprites
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('spaceship', './assets/small_spaceship.png');
+        this.load.image('small_spaceship', './assets/small_spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
@@ -28,11 +28,11 @@ class Play extends Phaser.Scene {
         // add Rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
-        // add Spaceships (x3)
+        // add Spaceships (x4)
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
-        this.shipSmall = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'small_spaceship', 0, 25).setOrigin(0,0);
+        this.shipSmall = new SmallSpaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'small_spaceship', 0, 0).setOrigin(0,0);
 
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
